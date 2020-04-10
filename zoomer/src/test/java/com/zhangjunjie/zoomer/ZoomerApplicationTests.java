1b9162c50e3c6b3f788eb52e17b31d541c64c1a4
张俊杰 <zhangjunjie@sensorsdata.cn>
2020-04-11 02:17:26 +0800

    init spring-boot project

---
+++ zoomer/src/test/java/com/zhangjunjie/zoomer/ZoomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangjunjie.zoomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
